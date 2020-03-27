4ffcc54ba78c78ece1aa0873c83b9ff1a9cb4943
xgh <1371818484@qq.com>
2020-03-27 23:30:09 +0800

    分支test

---
+++ laoma/src/main/java/com/test/C.java
@@ -0,0 +1,12 @@
+package com.test;
+
+/**
+ * @author xgh
+ * @date ：Created on 2020/3/27
+ */
+public class C {
+
+    public static void main(String[] args) {
+        System.out.println(1111);
+    }
+}
